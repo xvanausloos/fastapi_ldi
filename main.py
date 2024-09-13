b2736d59fa3683f92b930b3b17407c67e497dded
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-09-13 18:07:16 +0200

    payload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -6,4 +7,11 @@ app = FastAPI()
 async def root():
     return {"message": "hello world"}
 
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
 
+@app.post("/createposts")
+def create_posts(payload: dict = Body):
+    print(payload)
+    return {"new post": f"title { payload['title']} content: {payload['content']}"}
