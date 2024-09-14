c168a36bcb73a14f9ee95ea48fea90d4374b6e6d
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-09-14 16:20:03 +0200

    add posts/id function

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id":1}, {"title": "favorite foods", "content": "I like pizza", "id":2}]
 
 @app.get("/")
 async def root():
@@ -9,9 +20,16 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body):
-    print(payload)
-    return {"new post": f"title { payload['title']} content: {payload['content']}"}
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 100000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id):
+    print(id)
+    return {"post_detail": f"Here is the post {id}"}
