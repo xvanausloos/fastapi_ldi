1bcd9a1d2a9b31a5e98c69834d0c3943af590fdb
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-09-18 07:30:05 +0200

    fix import HTTPEXCEPTION

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Optional
-from fastapi import FastAPI, HTTPException, Response, status
+from fastapi import FastAPI, Response, status, HTTPException
+from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 
